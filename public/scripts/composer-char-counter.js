65e250c65a7d867e5ce00be38b17adf4e3213520
colspen <spencerlewiscole@gmail.com>
2022-10-11 18:48:06 +0000

    added a countergit add .git add .git add .git add .!

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $('#tweet-text').on('input', function() {
+    let counter = $(this).val().length;
+
+    $(this).siblings('.form-footer').children('.counter').text(140 - counter);
+    if (counter < 140) {
+      $('.counter').css("color", "#000000c8");
+    }
+    if (counter > 140) {
+      $(this).siblings('.form-footer').children('.counter').css("color", "firebrick");
+    }
+  });
+
+});
+
